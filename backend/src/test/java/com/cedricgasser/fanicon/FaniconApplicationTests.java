31077047608a45ab47224a693062c6efa267d15c
Cedric Gasser <cedric_gasser@sluz.ch>
2021-05-14 00:51:21 +0200

    Added dummy Test and changed .gitignore

--- backend/src/test/java/com/cedricgasser/fanicon/FaniconApplicationTests.java
+++ backend/src/test/java/com/cedricgasser/fanicon/FaniconApplicationTests.java
@@ -1,13 +1,15 @@
 package com.cedricgasser.fanicon;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
-class FaniconApplicationTests {
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FaniconApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void testNothing() {
+		assertThat(1).isEqualTo(1);
 	}
-
 }
