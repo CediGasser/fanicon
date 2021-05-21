91176e590bec885d62f91f182be5c4459e049eaf
Cedric Gasser <cedric_gasser@sluz.ch>
2021-05-21 17:51:15 +0200

    Add icon and theme model

---
+++ backend/src/main/java/com/cedricgasser/fanicon/theme/Theme.java
@@ -0,0 +1,21 @@
+package com.cedricgasser.fanicon.theme;
+
+import com.cedricgasser.fanicon.icon.Icon;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Theme {
+    @Id
+    @SequenceGenerator(name = "theme_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "theme_sequence")
+    @Column(name = "id", nullable = false, updatable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "theme")
+    private List<Icon> icon;
+}
