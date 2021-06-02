9de20881ef0fa6c7796bced5a521ea8b61502303
Cedric Gasser <cedric_gasser@sluz.ch>
2021-06-02 10:20:00 +0200

    Implement /users endpoint for admin

--- backend/src/main/java/com/cedricgasser/fanicon/model/User.java
+++ backend/src/main/java/com/cedricgasser/fanicon/model/User.java
@@ -42,4 +42,5 @@ public class User {
     public String getName() { return name; }
     public String getEmail() { return email; }
     public String getPassword() { return password; }
+    public void setPassword(String password){ this.password = password; }
 }
--- backend/src/main/java/com/cedricgasser/fanicon/service/UserService.java
+++ backend/src/main/java/com/cedricgasser/fanicon/service/UserService.java
@@ -1,7 +1,9 @@
 package com.cedricgasser.fanicon.service;
 
 import com.cedricgasser.fanicon.dto.DesignDto;
+import com.cedricgasser.fanicon.dto.UserDto;
 import com.cedricgasser.fanicon.model.Design;
+import com.cedricgasser.fanicon.model.Icon;
 import com.cedricgasser.fanicon.model.User;
 import com.cedricgasser.fanicon.model.UserGroup;
 import com.cedricgasser.fanicon.repository.DesignRepository;
@@ -13,6 +15,8 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Service
 public class UserService {
@@ -49,5 +53,18 @@ public class UserService {
             designDtos.add(new DesignDto(design.getId(), design.getName(), design.getBgColor(), design.getIconSize(), design.getIconMargin(), design.getUser().getName(), designService.getIconsFromDesignId(design.getId())));
         }
         return designDtos;
+    }
+
+    @Transactional(readOnly = true)
+    public List<User> getAll(){
+        Iterable<User> users = userRepository.findAll();
+        List<User> userList = StreamSupport
+                .stream(users.spliterator(), false)
+                .collect(Collectors.toList());
+
+        for (User user: userList) {
+            user.setPassword("");
+        }
+        return userList;
     }
 }
