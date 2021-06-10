f8fdd6cfba6304411f3520a4cda96238166986d1
Cedric Gasser <cedric_gasser@sluz.ch>
2021-06-11 00:49:37 +0200

    Implement color options

--- frontend/html/js/editor.js
+++ frontend/html/js/editor.js
@@ -11,11 +11,11 @@ btnSearchIcon.addEventListener('click', async () => {
 
     icons.forEach(icon => {
         str += `
-        <button data-id="${icon.id}"><div class="card cardStyle iconCard text-center">
+        <div class="card cardStyle iconCard text-center" data-id="${icon.id}">
             ${icon.svg}
             <h5 class="card-title">${icon.name}</h5>
             <h6 class="card-subtitle mb-2 text-muted">${icon.theme.name}</h6>
-        </div></button>`
+        </div>`
     })
 
 
@@ -23,7 +23,7 @@ btnSearchIcon.addEventListener('click', async () => {
 
     iconCardsContainer.innerHTML = str
 
-    let iconCards = iconCardsContainer.querySelectorAll('button')
+    let iconCards = iconCardsContainer.querySelectorAll('div[class="card cardStyle iconCard text-center"]');
     iconCards.forEach(card => {
         card.addEventListener ('click', addToDesign);
     });
@@ -32,8 +32,46 @@ btnSearchIcon.addEventListener('click', async () => {
 btnSearchIcon.click()
 
 function addToDesign(e){
-    debugger
-    console.log(e)
-    svg = e.target.querySelectorAll('svg')
+    let svg = e.target.closest('div').querySelector('svg').cloneNode(true)
     divDesign.appendChild(svg)
 }
+
+document.getElementById('inputBgColor').addEventListener("input", e => {
+    divDesign.style.backgroundColor = e.target.value
+});
+
+document.getElementById('inputIconColor').addEventListener("input", e =>{
+    let color = e.target.value
+    CCSStylesheetRuleStyle('styles', ".icon g", "fill", color);
+})
+
+function CCSStylesheetRuleStyle(stylesheet, selectorText, style, value){
+    /* returns the value of the element style of the rule in the stylesheet
+    *  If no value is given, reads the value
+    *  If value is given, the value is changed and returned
+    *  If '' (empty string) is given, erases the value.
+    *  The browser will apply the default one
+    *
+    * string stylesheet: part of the .css name to be recognized, e.g. 'default'
+    * string selectorText: css selector, e.g. '#myId', '.myClass', 'thead td'
+    * string style: camelCase element style, e.g. 'fontSize'
+    * string value optionnal : the new value
+    */
+    var CCSstyle = undefined, rules;
+    for(var m in document.styleSheets){
+      if(document.styleSheets[m].ownerNode.id == stylesheet){
+       rules = document.styleSheets[m][document.all ? 'rules' : 'cssRules'];
+       for(var n in rules){
+         if(rules[n].selectorText == selectorText){
+           CCSstyle = rules[n].style;
+           break;
+         }
+       }
+       break;
+      }
+    }
+    if(value == undefined)
+      return CCSstyle[style]
+    else
+      return CCSstyle[style] = value
+  }
